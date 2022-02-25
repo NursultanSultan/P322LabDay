ddcb6bc23e27d46296889a53ccbcbce8dfd318e1
NursultanSultan <tu201906251@code.edu.az>
2022-02-25 09:44:38 -0800

    I Added Lab day taks

---
+++ Labday2022task/Labday2022task/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Labday2022task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int num1 = 2020, num2 = 2025, num3 = 2023;
+
+
+            //if (num1 + num2 + num3 == 2022)
+            //{
+            //    Console.WriteLine("doqrudur");
+            //}
+            //else if (num1 - num2 - num3 == 2022)
+            //{
+            //    Console.WriteLine("doqrudur");
+            //}
+            //else if (num1 + num2 - num3 == 2022)
+            //{
+            //    Console.WriteLine("doqrudur");
+            //}
+            //else if (num1 - num2 + num3 == 2022)
+            //{
+            //    Console.WriteLine("doqrudur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yanlisdir");
+            //}
+            #endregion
+
+            #region Task 2
+            //int month = 7;
+
+            //if (month < 1 || month > 12)
+            //{
+            //    Console.WriteLine("Bele bir ay movcud deyil");
+            //}
+            //else if(month < 3 || month == 12)
+            //{
+            //    Console.WriteLine("Winter");
+            //}
+            //else if (month <= 5)
+            //{
+            //    Console.WriteLine("Yaz");
+            //}
+            //else if (month <= 8)
+            //{
+            //    Console.WriteLine("Summer");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Payiz");
+            //}
+            #endregion
+        }
+    }
+}
